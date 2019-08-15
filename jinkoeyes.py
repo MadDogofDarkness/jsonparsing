1b2364cc27455e85ed58e021431aa395c52f147a
MadDogofDarkness <52047130+MadDogofDarkness@users.noreply.github.com>
2019-08-15 10:32:03 -0700

    Add files via upload

--- jinkoeyes.py
+++ jinkoeyes.py
@@ -1,11 +1,31 @@
-from lxml import html
+from html.parser import HTMLParser
 import requests
+
+class Myparser(HTMLParser):
+
+        def handle_starttag(self, tag, attrs):
+                if tag == 'meta':
+                        print(f"tag: {tag} : {attrs}")
+                else:
+                        pass
+
+        def handle_endtag(self, tag):
+                #print("end tag: ", tag)
+                pass
+
+        def handle_data(self, data):
+                if self.get_starttag_text() == 'meta':
+                        print(data)
+                
+
+def getmeta(htmlinput):
+        parser = Myparser()
+        parser.feed(htmlinput)
+        print('success!')
 
 def look(siteurl):
     page = requests.get(str(siteurl))
-    tree = html.fromstring(page.content)
-    print(tree)
-    results = []
+    print(page)
     return page.text
 
 def save(data):
@@ -17,4 +37,6 @@ def save(data):
 
 if __name__ == '__main__':
     url = input("input url: ")
-    print(save(look(url)))
+    #getmeta(look(url))
+    look(url)
+
